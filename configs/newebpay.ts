0f30e3da97444dc4a551b7693a37642cd028aafa
Jamie Lin <jamie_lin@adata.com>
2022-01-26 15:11:35 +0800

    refactor: extract NormalPayForm

--- configs/newebpay.ts
+++ configs/newebpay.ts
@@ -3,3 +3,17 @@ export enum SupportedLanguage {
   ENGLISH = 'en',
   JAPANESE = 'jp',
 }
+export const languageOptions = [
+  {
+    label: '繁體中文',
+    value: SupportedLanguage.TRADITIONAL_CHINESE,
+  },
+  {
+    label: '英文',
+    value: SupportedLanguage.ENGLISH,
+  },
+  {
+    label: '日文',
+    value: SupportedLanguage.JAPANESE,
+  },
+]
